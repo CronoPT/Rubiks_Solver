8f02895e346fc2862661262252de0b8681debb7e
CronoPT <vasco.dias.silva@tecnico.ulisboa.pt>
2019-06-28 14:58:30 +0100

    Refactor rotate_bottom_counter_clock_wise.h

--- cube_actions/rotate_bottom_counter_clock_wise.h
+++ cube_actions/rotate_bottom_counter_clock_wise.h
@@ -10,13 +10,14 @@
 |   '-m----m-'                                                       |
 ====================================================================*/
 
-#ifndef __ROTATEBOTTOMCOUNTERCLOCKWISE_H__
-#define __ROTATEBOTTOMCOUNTERCLOCKWISE_H__
+#ifndef __ROTATE_BOTTOM_COUNTER_CLOCK_WISE_H__
+#define __ROTATE_BOTTOM_COUNTER_CLOCK_WISE_H__
 
-#include "../Search/Action.h"
-#include "../RubiksCube.h"
+#include "../search/action.h"
+#include "../rubiks_cube.h"
 
-using namespace std;
+namespace cubing
+{
 
 /*==============================================================
 | class: rotateBottomCounterClockWise - squares is the number of 
@@ -25,33 +26,35 @@ using namespace std;
 | diferent from 1, since it does not make sense to rotate
 | beyond the middle
 ==============================================================*/
-class RotateBottomCounterClockWise: public Action<RubiksCube>
+class rotate_bottom_counter_clock_wise: public action<rubiks_cube>
 {
     private:
         int _squares;
 
     public:
-        RotateBottomCounterClockWise(int squares): Action<RubiksCube>()
+        rotate_bottom_counter_clock_wise(int squares): action<rubiks_cube>()
         {   
             _squares = squares;
         }
 
-        RubiksCube execute(RubiksCube cube) override
+        rubiks_cube execute(rubiks_cube cube) override
         {
-            cube.rotateFaceCounterClockWise(BOTTOM, _squares);
+            cube.rotate_face_counter_clock_wise(BOTTOM, _squares);
             return cube;
         }
 
-        void dumpTo(ostream& os) const override
+        void dump_to(ostream& os) const override
         {
             os << "Bottom Counter " << _squares;
         }
 
-        friend ostream& operator<<(ostream& os, const RotateBottomCounterClockWise& r)
+        friend ostream& operator<<(ostream& os, const rotate_bottom_counter_clock_wise& r)
         {
-            r.dumpTo(os);
+            r.dump_to(os);
             return os;
         }
 };
+
+} //cubing
 
 #endif
