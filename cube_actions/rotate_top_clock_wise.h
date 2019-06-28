abe4a4fb82384eaba47d0d7f3502b44abd02c607
CronoPT <vasco.dias.silva@tecnico.ulisboa.pt>
2019-06-28 15:25:15 +0100

    Refactor rotate_top_clock_wise.h

--- cube_actions/rotate_top_clock_wise.h
+++ cube_actions/rotate_top_clock_wise.h
@@ -10,13 +10,14 @@
 |   '-m----m-'                                                       |
 ====================================================================*/
 
-#ifndef __ROTATETOPCLOCKWISE_H__
-#define __ROTATETOPCLOCKWISE_H__
+#ifndef __ROTATE_TOP_CLOCK_WISE_H__
+#define __ROTATE_TOP_CLOCK_WISE_H__
 
-#include "../Search/Action.h"
-#include "../RubiksCube.h"
+#include "../search/action.h"
+#include "../rubiks_cube.h"
 
-using namespace std;
+namespace cubing
+{
 
 /*==============================================================
 | class: rotateTopClockWise - squares is the number of squares
@@ -25,33 +26,35 @@ using namespace std;
 | diferent from 1, since it does not make sense to rotate
 | beyond the middle
 ==============================================================*/
-class RotateTopClockWise: public Action<RubiksCube>
+class rotate_top_clock_wise: public action<rubiks_cube>
 {
     private:
         int _squares;
 
     public:
-        RotateTopClockWise(int squares): Action<RubiksCube>()
+        rotate_top_clock_wise(int squares): action<rubiks_cube>()
         {   
             _squares = squares;
         }
         
-        RubiksCube execute(RubiksCube cube) override
+        rubiks_cube execute(rubiks_cube cube) override
         {
-            cube.rotateFaceClockWise(TOP, _squares);
+            cube.rotate_face_clock_wise(TOP, _squares);
             return cube;
         }
 
-        void dumpTo(ostream& os) const override
+        void dump_to(ostream& os) const override
         {
             os << "Top Clock " << _squares;
         }
 
-        friend ostream& operator<<(ostream& os, const RotateTopClockWise& r)
+        friend ostream& operator<<(ostream& os, const rotate_top_clock_wise& r)
         {
-            r.dumpTo(os);
+            r.dump_to(os);
             return os;
         }
 };
+
+} //cubing
 
 #endif
