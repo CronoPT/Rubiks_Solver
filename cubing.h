f5ac191f8c2945890a98945418ed580a5d151440
CronoPT <vasco.dias.silva@tecnico.ulisboa.pt>
2019-06-28 14:35:36 +0100

    Refactor cubing.h

--- cubing.h
+++ cubing.h
@@ -13,84 +13,89 @@
 #ifndef __CUBING_H__
 #define __CUBING_H__
 
-#include "Search/Problem.h"
-#include "Search/Action.h"
-#include "RubiksCube.h"
+#include "search/problem.h"
+#include "search/section.h"
+#include "rubiks_cube.h"
 
-#include "CubeActions/RotateBackClockWise.h"
-#include "CubeActions/RotateBackCounterClockWise.h"
-#include "CubeActions/RotateFrontClockWise.h"
-#include "CubeActions/RotateFrontCounterClockWise.h"
-#include "CubeActions/RotateTopClockWise.h"
-#include "CubeActions/RotateTopCounterClockWise.h"
-#include "CubeActions/RotateBottomClockWise.h"
-#include "CubeActions/RotateBottomCounterClockWise.h"
-#include "CubeActions/RotateRightClockWise.h"
-#include "CubeActions/RotateRightCounterClockWise.h"
-#include "CubeActions/RotateLeftClockWise.h"
-#include "CubeActions/RotateLeftCounterClockWise.h"
+#include "cube_actions/RotateBackClockWise.h"
+#include "cube_actions/RotateBackCounterClockWise.h"
+#include "cube_actions/RotateFrontClockWise.h"
+#include "cube_actions/RotateFrontCounterClockWise.h"
+#include "cube_actions/RotateTopClockWise.h"
+#include "cube_actions/RotateTopCounterClockWise.h"
+#include "cube_actions/RotateBottomClockWise.h"
+#include "cube_actions/RotateBottomCounterClockWise.h"
+#include "cube_actions/RotateRightClockWise.h"
+#include "cube_actions/RotateRightCounterClockWise.h"
+#include "cube_actions/RotateLeftClockWise.h"
+#include "cube_actions/RotateLeftCounterClockWise.h"
 
 #include <memory>
 
-class Cubing: public Problem<RubiksCube>
+namespace cubing 
+{
+
+class cubing: public problem<rubiks_cube>
 {
     public:
-        Cubing(RubiksCube cube): Problem<RubiksCube>(cube) {}
+        cubing(rubiks_cube cube): problem<rubiks_cube>(cube) {}
 
-        vector<shared_ptr<Action<RubiksCube>>> actions(RubiksCube cube) override
+        vector<shared_ptr<action<rubiks_cube>>> actions(rubiks_cube cube) override
         {   
-            vector<shared_ptr<Action<RubiksCube>>> res;
-            for(int i=1; i<=(int)cube.getN()/2; i++)
+            vector<shared_ptr<action<rubiks_cube>>> res;
+            for(int i=1; i<=(int)cube.N()/2; i++)
             {
-                res.push_back(make_shared<RotateBackClockWise>(RotateBackClockWise(i)));
-                res.push_back(make_shared<RotateBackCounterClockWise>(RotateBackCounterClockWise(i)));
-                res.push_back(make_shared<RotateFrontClockWise>(RotateFrontClockWise(i)));
-                res.push_back(make_shared<RotateFrontCounterClockWise>(RotateFrontCounterClockWise(i)));
-                res.push_back(make_shared<RotateTopClockWise>(RotateTopClockWise(i)));
-                res.push_back(make_shared<RotateTopCounterClockWise>(RotateTopCounterClockWise(i)));
-                res.push_back(make_shared<RotateBottomClockWise>(RotateBottomClockWise(i)));
-                res.push_back(make_shared<RotateBottomCounterClockWise>(RotateBottomCounterClockWise(i)));
-                res.push_back(make_shared<RotateRightClockWise>(RotateRightClockWise(i)));
-                res.push_back(make_shared<RotateRightCounterClockWise>(RotateRightCounterClockWise(i)));
-                res.push_back(make_shared<RotateLeftClockWise>(RotateLeftClockWise(i)));
-                res.push_back(make_shared<RotateLeftCounterClockWise>(RotateLeftCounterClockWise(i))); 
+                res.push_back(make_shared<rotate_back_clock_wise>(rotate_back_clock_wise(i)));
+                res.push_back(make_shared<rotate_back_counter_clock_wise>(rotate_back_counter_clock_wise(i)));
+                res.push_back(make_shared<rotate_front_clock_wise>(rotate_front_clock_wise(i)));
+                res.push_back(make_shared<rotate_front_counter_clock_wise>(rotate_front_counter_clock_wise(i)));
+                res.push_back(make_shared<rotate_top_clock_wise>(rotate_top_clock_wise(i)));
+                res.push_back(make_shared<rotate_top_counter_clock_wise>(rotate_top_counter_clock_wise(i)));
+                res.push_back(make_shared<rotate_bottom_clock_wise>(rotate_bottom_clock_wise(i)));
+                res.push_back(make_shared<rotate_bottom_counter_clock_wise>(rotate_bottom_counter_clock_wise(i)));
+                res.push_back(make_shared<rotate_right_clock_wise>(rotate_right_clock_wise(i)));
+                res.push_back(make_shared<rotate_right_counter_clock_wise>(rotate_right_counter_clock_wise(i)));
+                res.push_back(make_shared<rotate_left_clock_wise>(rotate_left_clock_wise(i)));
+                res.push_back(make_shared<rotate_left_counter_clock_wise>(rotate_left_counter_clock_wise(i))); 
             }
 
             return res;
         }
 
-        bool goalTest(RubiksCube cube) override
+        bool goal_test(rubiks_cube cube) override
         {
             return cube.solved();
         }
 
-        double heuristic(RubiksCube cube) const override
+        double heuristic(rubiks_cube cube) const override
         {
             return 0;
             if(cube.solved())
                 return 0;
 
             int to_subtract = 0;
-            int n = cube.getN();
-            for(Face face : cube.getFaces())
+            int n = cube.N();
+            for(face face : cube.faces())
                 for(int l=0; l<n; l++)
                     for(int c=0; c<n; c++)
                     {
                         if(c > 1)
-                            if(face.getPosition(l, c) == face.getPosition(l, c-1))
+                            if(face.position(l, c) == face.position(l, c-1))
                                 to_subtract++;
                         if(c < n-1)
-                            if(face.getPosition(l, c) == face.getPosition(l, c+1))
+                            if(face.position(l, c) == face.position(l, c+1))
                                 to_subtract++;
                         if(l > 1)
-                            if(face.getPosition(l, c) == face.getPosition(l-1, c))
+                            if(face.position(l, c) == face.position(l-1, c))
                                 to_subtract++;
                         if(l < n-1)
-                            if(face.getPosition(l, c) == face.getPosition(l+1, c))
+                            if(face.position(l, c) == face.position(l+1, c))
                                 to_subtract++;
                     }
             return n*n*6*4 - to_subtract;
         }
 };
+
+} //cubing
 
 #endif
